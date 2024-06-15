ec71d0dbcaf86900b22efdc5826e86dcdf934b65
Shimada Ryuga <shimadaryuga@gmail.com>
2024-06-15 18:23:13 +0900

    リンク表示

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -22,26 +22,205 @@ class MusicsController < ApplicationController
     end
   end
 
-  def recommend
-    @music = session[:music]
-    @genre = session[:genre]
+  def happy
+    music = Music.where(emotion: 'happy').order(Arel.sql('RANDOM()')).first
+    session[:music] = {
+      title: music.title,
+      artist: music.artist,
+      rylic: music.rylic,
+      appleMusic: music.appleMusic,
+      spotify: music.spotify,
+      youTube: music.youTube,
+    }
+    session[:genre] = "なんとなく楽しい"
+    redirect_to '/recommend'
+  end
+
+  def sad
+    music = Music.where(emotion: 'sad').order(Arel.sql('RANDOM()')).first
+    session[:music] = {
+      title: music.title,
+      artist: music.artist,
+      rylic: music.rylic,
+      appleMusic: music.appleMusic,
+      spotify: music.spotify,
+      youTube: music.youTube,
+    }
+    session[:genre] = "なんとなく悲しい"
+    redirect_to '/recommend'
+  end
+
+  def badMorning
+    music = Music.where(emotion: 'badMorning').order(Arel.sql('RANDOM()')).first
+    session[:music] = {
+      title: music.title,
+      artist: music.artist,
+      rylic: music.rylic,
+      appleMusic: music.appleMusic,
+      spotify: music.spotify,
+      youTube: music.youTube,
+    }
+    session[:genre] = "時間のない朝"
+    redirect_to '/recommend'
   end
 
   def confused
-    session[:music] = Music.all.find_by(emotion: "confused")
+    music = Music.where(emotion: 'confused').order(Arel.sql('RANDOM()')).first
+    session[:music] = {
+      title: music.title,
+      artist: music.artist,
+      rylic: music.rylic,
+      appleMusic: music.appleMusic,
+      spotify: music.spotify,
+      youTube: music.youTube,
+    }
     session[:genre] = "迷っている"
     redirect_to '/recommend'
   end
 
-  def ready
-    session[:post] = ["Touch off", "銀河高速"].sample
-    session[:genre] = "俺はもう覚悟決めた"
+  def goodDay
+    music = Music.where(emotion: 'goodDay').order(Arel.sql('RANDOM()')).first
+    session[:music] = {
+      title: music.title,
+      artist: music.artist,
+      rylic: music.rylic,
+      appleMusic: music.appleMusic,
+      spotify: music.spotify,
+      youTube: music.youTube,
+    }
+    session[:genre] = "気持ちのいい昼"
+    redirect_to '/recommend'
+  end
+
+  def badDay
+    music = Music.where(emotion: 'badDay').order(Arel.sql('RANDOM()')).first
+    session[:music] = {
+      title: music.title,
+      artist: music.artist,
+      rylic: music.rylic,
+      appleMusic: music.appleMusic,
+      spotify: music.spotify,
+      youTube: music.youTube,
+    }
+    session[:genre] = "気分の上がらない昼"
+    redirect_to '/recommend'
+  end
+
+  def goodEvening
+    music = Music.where(emotion: 'goodEvening').order(Arel.sql('RANDOM()')).first
+    session[:music] = {
+      title: music.title,
+      artist: music.artist,
+      rylic: music.rylic,
+      appleMusic: music.appleMusic,
+      spotify: music.spotify,
+      youTube: music.youTube,
+    }
+    session[:genre] = "澄んだ気持ちの夕方"
     redirect_to '/recommend'
   end
 
   def giveup
-    session[:music] = Music.all.find_by(emotion: "giveup")
+    music = Music.where(emotion: 'giveup').order(Arel.sql('RANDOM()')).first
+    session[:music] = {
+      title: music.title,
+      artist: music.artist,
+      rylic: music.rylic,
+      appleMusic: music.appleMusic,
+      spotify: music.spotify,
+      youTube: music.youTube,
+    }
     session[:genre] = "全部投げ出したい"
     redirect_to '/recommend'
+  end
+
+  def goodMorning
+    music = Music.where(emotion: 'goodMorning').order(Arel.sql('RANDOM()')).first
+    session[:music] = {
+      title: music.title,
+      artist: music.artist,
+      rylic: music.rylic,
+      appleMusic: music.appleMusic,
+      spotify: music.spotify,
+      youTube: music.youTube,
+    }
+    session[:genre] = "余裕のある朝"
+    redirect_to '/recommend'
+  end
+
+  def goodNight
+    music = Music.where(emotion: 'goodNight').order(Arel.sql('RANDOM()')).first
+    session[:music] = {
+      title: music.title,
+      artist: music.artist,
+      rylic: music.rylic,
+      appleMusic: music.appleMusic,
+      spotify: music.spotify,
+      youTube: music.youTube,
+    }
+    session[:genre] = "楽しかった夜"
+    redirect_to '/recommend'
+  end
+
+  def badNight
+    music = Music.where(emotion: 'badNight').order(Arel.sql('RANDOM()')).first
+    session[:music] = {
+      title: music.title,
+      artist: music.artist,
+      rylic: music.rylic,
+      appleMusic: music.appleMusic,
+      spotify: music.spotify,
+      youTube: music.youTube,
+    }
+    session[:genre] = "眠れない夜"
+    redirect_to '/recommend'
+  end
+
+  def badEvening
+    music = Music.where(emotion: 'badEvening').order(Arel.sql('RANDOM()')).first
+    session[:music] = {
+      title: music.title,
+      artist: music.artist,
+      rylic: music.rylic,
+      appleMusic: music.appleMusic,
+      spotify: music.spotify,
+      youTube: music.youTube,
+    }
+    session[:genre] = "ちょっと後悔の夕方"
+    redirect_to '/recommend'
+  end
+
+  def motivated
+    music = Music.where(emotion: 'motivated').order(Arel.sql('RANDOM()')).first
+    session[:music] = {
+      title: music.title,
+      artist: music.artist,
+      rylic: music.rylic,
+      appleMusic: music.appleMusic,
+      spotify: music.spotify,
+      youTube: music.youTube,
+    }
+    session[:genre] = "やる気に満ち溢れている"
+    redirect_to '/recommend'
+  end
+
+  def hiTension
+    music = Music.where(emotion: 'hiTension').order(Arel.sql('RANDOM()')).first
+    session[:music] = {
+      title: music.title,
+      artist: music.artist,
+      rylic: music.rylic,
+      appleMusic: music.appleMusic,
+      spotify: music.spotify,
+      youTube: music.youTube,
+    }
+    session[:genre] = "テンションを上げたい"
+    redirect_to '/recommend'
+  end
+
+  def recommend
+    music_data = session[:music]
+    @music = Music.new(music_data) if music_data
+    @genre = session[:genre]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
   root 'musics#top'
   get 'recommend' => "musics#recommend"
-  get 'recommend/ready' => "musics#ready"
+  get 'recommend/happy' => "musics#happy"
+  get 'recommend/badDay' => "musics#badDay"
+  get 'recommend/sad' => "musics#sad"
+  get 'recommend/badMorning' => "musics#badMorning"
   get 'recommend/confused' => "musics#confused"
+  get 'recommend/goodDay' => "musics#goodDay"
+  get 'recommend/goodEvening' => "musics#goodEvening"
   get 'recommend/giveup' => "musics#giveup"
+  get 'recommend/goodMorning' => "musics#goodMorning"
+  get 'recommend/goodNight' => "musics#goodNight"
+  get 'recommend/badNight' => "musics#badNight"
+  get 'recommend/badEvening' => "musics#badEvening"
+  get 'recommend/motivated' => "musics#motivated"
+  get 'recommend/hiTension' => "musics#hiTension"
   get "musics/new" => "musics#new"
   post "musics/create" => "musics#create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
